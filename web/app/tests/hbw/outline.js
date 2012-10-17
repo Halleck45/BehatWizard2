5f572e45c1b8252f0f5e94a1acc47b3a910a0251
Halleck45 <jeanfrancois@lepine.pro>
2012-10-17 10:16:34 +0200

    tests of entities are green

--- web/app/tests/hbw/outline.js
+++ web/app/tests/hbw/outline.js
@@ -1,11 +1,11 @@
 test('outline can be converted to string', function() {
 
     var outline1 = new hbw.entity.outline([
-        {name:"Lepine",firstname:"jean-Francois"},
-        {name:"Foo",firstname:"Bar"}
+        ["Lepine","Jean-Francois"],
+        ["Foo","Bar"]
     ]);
 
-    equal(outline1.asString(), "| Lepine | Jean-Francois |\n| Foo | Bar |", 'Example is converted to string');
+    equal(outline1.asString(), "\n      | Lepine | Jean-Francois |\n      | Foo | Bar |", 'Example is converted to string');
 });
 
 test('Rows given to an outline node are checked', function() {
