1570f64e691c66824d1e0f9a9f233918ea69b12c
Halleck45 <jeanfrancois@lepine.pro>
2012-10-23 18:26:49 +0200

    user can filter its feature by title, tag or(and) state

--- web/app/controllers/FeatureListController.js
+++ web/app/controllers/FeatureListController.js
@@ -4,4 +4,44 @@ function FeatureListController($scope, FeatureService, $http) {
         FeatureService.setFeatures(data);
         $scope.feature = FeatureService.getCurrentFeature();
     });
+    
+    $scope.criterium = {
+        state : null,
+        text : null,
+        tag : null
+    }
+    
+    $scope.setCriterium = function(type, value) {
+        $scope.criterium[type] = value;
+    }
+    
+    $scope.matchCurrentCriterium = function(feature) {
+        
+        // state
+        if(null !== $scope.criterium.state && $scope.criterium.state != feature.state) {
+            return false;
+        }
+        
+        // title
+        if(null !== $scope.criterium.text && feature.title.indexOf($scope.criterium.text) < 0) {
+            return false;
+        }
+        
+        // tags
+        if(null != $scope.criterium.tag) {
+            var i, found;
+            for(i in feature.tags) {
+                if(feature.tags[i] == $scope.criterium.tag) {
+                    found = true;
+                    break;
+                }
+            }
+            if(!found) {
+                return false;
+            }
+        }
+        return true;
+    };
+    
+    
 }
