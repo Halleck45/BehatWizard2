b10dbc0b84307f666891e077a53bfc338dadb568
Halleck45 <jeanfrancois@lepine.pro>
2012-10-23 18:17:05 +0200

    user can list its features - ui

--- web/app/controllers/FeatureListController.js
+++ web/app/controllers/FeatureListController.js
@@ -2,5 +2,6 @@ function FeatureListController($scope, FeatureService, $http) {
     $http.get('features.json').success(function(data) {
         $scope.features = data;
         FeatureService.setFeatures(data);
+        $scope.feature = FeatureService.getCurrentFeature();
     });
 }
